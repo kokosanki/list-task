47d0a90c56ce9a62d1b83a90baff08ce3c20da8a
kokosanki <os_mo@wp.pl>
2020-08-02 21:15:00 +0200

    vuetify fixes

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -2,10 +2,11 @@
   <v-container>
     <div>
       <h2 class="text-center">{{ $route.params.id }}</h2>
-      <LoremIpsum add="1p" />
+      <LoremIpsum class="text-justify" add="1p" />
       <div class="d-flex flex-row justify-space-between">
         <v-icon color="#29006b" @click="$router.go(-1)" large>keyboard_backspace</v-icon>
-        <v-icon color="#29006b" @click="$router.push('/')" large>home</v-icon>
+          <v-icon color="#29006b"
+        @click="$router.push('/')" large>home</v-icon>
       </div>
     </div>
   </v-container>
@@ -24,5 +25,12 @@ a {
   color: blue;
   font-weight: bold;
   cursor: pointer;
+}
+.v-icon:hover {
+  &::after {
+  background: #29006b;
+  opacity: 0.12;
+  border-radius: 50%;
+  }
 }
 </style>
